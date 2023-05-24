b747816c74be9a2e2ac96249226baed6a841770f
JBGruber <gruber@europa-uni.de>
2023-05-24 23:19:39 +0200

    small correction

--- cases/4-2/make.sh
+++ cases/4-2/make.sh
@@ -5,6 +5,9 @@ Rscript install_dependencies.r
 Rscript get_material.r
 Rscript install_dependencies.r
 
+# chnages to get it to run
+## correct wrong data path
+sed -i 's|data/|../data/|g' code_for_reproduction/repro.Rmd
 
 # run analysis
 Rscript -e "rmarkdown::render('./code_for_reproduction/repro.Rmd')"
