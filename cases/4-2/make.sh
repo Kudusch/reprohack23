01b42b3ce53be9b5bc03d14a3dd8aa20b7ce7b96
chainsawriot <chainsawtiney@gmail.com>
2023-05-25 19:04:09 +0200

    Update `make.sh`
    Remove `rm` (not needed)
    
    Remove `cp` (so that it won't copy the `input` directory)

--- cases/4-2/make.sh
+++ cases/4-2/make.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# recreate clean output folder
-rm -r /usr/local/src/output/*
-
 # set up
 Rscript install_dependencies.r
 Rscript get_material.r
@@ -15,5 +12,3 @@ sed -i 's|data/|../data/|g' code_for_reproduction/repro.Rmd
 # run analysis
 Rscript -e "rmarkdown::render('./code_for_reproduction/repro.Rmd',
             output_dir = '/usr/local/src/output')"
-
-cp -r . ../output
