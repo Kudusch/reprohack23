edfc483b2f77a1c323124edc2dfd0968ec80b368
chainsawriot <chainsawtiney@gmail.com>
2023-07-05 13:12:12 +0200

    Add case 47
    Basically let it run and report status 1

---
+++ cases/47/make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set up
+# Rscript install_dependencies.r
+# Rscript get_material.r
+# Rscript install_dependencies.r
+
+git init
+git remote add origin https://github.com/Christoph/MultilingualTextAnalysis
+git fetch
+git checkout 6ff36427203738c7980999bfd38409434a221fb7
+
+## If you need python
+eval "$(pyenv init -)"
+pyenv global $PYTHON_VERSION
+pip install pipreqs jupyterlab notebook nbconvert
+## If there are notebooks
+##jupyter nbconvert *.ipynb --to script
+pipreqs --mode no-pin --force
+pip install -r requirements.txt
+pip freeze > /usr/local/src/output/requirements.txt
+##python --version
+
+## status 1
+python classifier_evaluation.py
