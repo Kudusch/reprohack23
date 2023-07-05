8a3d2908ec572d93b957a5e24bb7e7b01f0b12ee
chainsawriot <chainsawtiney@gmail.com>
2023-07-05 10:27:38 +0200

    Fix #8

---
+++ template/make.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# set up
+Rscript install_dependencies.r
+Rscript get_material.r
+Rscript install_dependencies.r
+
+## If you need python
+# eval "$(pyenv init -)"
+# pyenv global $PYTHON_VERSION
+# pip install pipreqs jupyterlab notebook nbconvert
+# ## If there are notebooks
+# jupyter nbconvert *.ipynb --to script
+# pipreqs --mode no-pin --force
+# pip install -r requirements.txt
+# pip freeze > /usr/local/src/output/requirements.txt
+# python --version
+
+## ADD THE BATCH EXECUTION CODE HERE
+## e.g.
+
+# Rscript -e "rmarkdown::render('repro.Rmd',
+#             output_dir = '/usr/local/src/output')"
