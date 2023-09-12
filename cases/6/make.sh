f7bf33b91da0837ac52f5642856f183cb83c6c4a
chainsawriot <chainsawtiney@gmail.com>
2023-09-12 13:57:59 +0200

    Add all patches for saving vis files

--- cases/6/make.sh
+++ cases/6/make.sh
@@ -19,11 +19,11 @@ parallel < cmds1.sh
 
 ## do all visualization tasks
 
-cd Simulation_BS_W1; R CMD BATCH Visualize_Progress_bs.R; cd ..
-cd Simulation_BS_W2; R CMD BATCH Visualize_Progress_bs2.R; cd ..
-cd Simulation_Integ_W1; R CMD BATCH Visualize_Progress_within.R; cd ..
-cd Simulation_Integ_W2; R CMD BATCH Visualize_Progress_within_w2.R; cd ..
-cd Simulation_Naive_W1; R CMD BATCH Visualize_Progress_complete.R; cd ..
+cp BS_W1.diff Simulation_BS_W1; cd Simulation_BS_W1; patch < BS_W1.diff; R CMD BATCH Visualize_Progress_bs.R; cd ..
+cp BS_W2.diff Simulation_BS_W2; cd Simulation_BS_W2; patch < BS_W2.diff; R CMD BATCH Visualize_Progress_bs2.R; cd ..
+cp Integ_W1.diff Simulation_Integ_W1; cd Simulation_Integ_W1; patch < Integ_W1.diff; R CMD BATCH Visualize_Progress_within.R; cd ..
+cp Integ_W2.diff Simulation_Integ_W2; cd Simulation_Integ_W2; patch < Integ_W2.diff; R CMD BATCH Visualize_Progress_within_w2.R; cd ..
+cp Naive_W1.diff Simulation_Naive_W1; cd Simulation_Naive_W1; patch < Naive_W1.diff; R CMD BATCH Visualize_Progress_complete.R; cd ..
 
 ## For simplicity, just copy all things
-cp -r /usr/local/src/input /usr/local/scr/output
+cp -r /usr/local/src/input /usr/local/src/output
